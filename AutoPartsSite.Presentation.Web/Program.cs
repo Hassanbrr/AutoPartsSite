505e28114540db7af0ec08047bbc17b872d2e990
Hassan_b <hasanbahmandar5@gmail.com>
2025-05-24 17:55:59 +0330

    اضافه کردن نوبار به بخش clint و بخش ckeditor همچنان باک دارد

--- AutoPartsSite.Presentation.Web/Program.cs
+++ AutoPartsSite.Presentation.Web/Program.cs
@@ -24,7 +24,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapControllerRoute(
         name: "default",
-        pattern: "{area=Admin}/{controller=Dashboard}/{action=Index}/{id?}")
+        pattern: "{area=Client}/{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 //app.MapControllerRoute(
--- AutoPartsSite.Presentation.Web/wwwroot/Client/js/site.js
+++ AutoPartsSite.Presentation.Web/wwwroot/Client/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const toggleBtn = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const search = document.querySelector('.search-container');
+const authBtn = document.querySelector('.btn-auth');
 
-// Write your JavaScript code.
+toggleBtn.addEventListener('click', () => {
+	navLinks.classList.toggle('active');
+	search.classList.toggle('active');
+	authBtn.classList.toggle('active');
+});
