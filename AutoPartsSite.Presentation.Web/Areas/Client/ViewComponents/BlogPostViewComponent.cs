952e6aa3229e9dcd41da0c8e90c54f9fd9b34e57
Hassan_b <hasanbahmandar5@gmail.com>
2025-06-01 23:53:45 +0330

    qwe

---
+++ AutoPartsSite.Presentation.Web/Areas/Client/ViewComponents/BlogPostViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AutoPartsSite.Application.InterFaces;
+using AutoPartsSite.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoPartsSite.Presentation.Web.Areas.Client.ViewComponents
+{
+    public class BlogPostViewComponent : ViewComponent
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogPostViewComponent(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _blogService.GetAllPostsAsync();
+            return View(posts);
+        }
+    }
+}
